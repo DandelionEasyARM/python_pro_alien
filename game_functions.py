061cefe600621cdc874b6e60b1e4ebf707fab767
wumingming <694021065@qq.com>
2017-05-17 02:30:08 +0800

    [学习内容]外星人入侵项目实践  1.重写事件检测函数，分别封装按键按下和释放检测

--- game_functions.py
+++ game_functions.py
@@ -4,22 +4,30 @@ import sys
 import pygame
 
 
+def check_keyup_event(event, ship):
+    """检测按键释放"""
+    ship.stop()
+
+def check_keydown_event(event, ship):
+    """检测按键按下"""
+    if pygame.K_RIGHT == event.key:
+        # 向右移动飞船
+        ship.moving('right')
+        ship.update()
+    if pygame.K_LEFT == event.key:
+        # 向左移动飞船
+        ship.moving('left')
+        ship.update()
+
 def check_event(ship):
     #监视键盘和鼠标事件
     for event in pygame.event.get():
         if pygame.QUIT == event.type:
             sys.exit()
         elif pygame.KEYDOWN == event.type:
-            if pygame.K_RIGHT == event.key:
-                #向右移动飞船
-                ship.moving('right')
-                ship.update()
-            if pygame.K_LEFT == event.key:
-                #向左移动飞船
-                ship.moving('left')
-                ship.update()
+            check_keydown_event(event, ship)
         elif pygame.KEYUP == event.type:
-            ship.stop()
+            check_keyup_event(event, ship)
 
 def update_scrren(ai_settings, scrren, ship):
     """更新屏幕上的图像"""
